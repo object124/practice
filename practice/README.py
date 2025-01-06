604e7d70f90bea6668200394b9ee32937d51ae65
junhyeungKim <zecc4504@gmail.com>
2025-01-06 18:02:46 +0900

    조건문 연 습 파일기록

--- practice/README.py
+++ practice/README.py
@@ -54,10 +54,162 @@
 #    result = result + i
 #print(result)
 
-a = 20
-print (a)
-b= 10
-print (b)
-sum = a + b
-print = (sum) 
+#a = 20
+#print (a)
+#b= 10
+#print (b)
+#sum = a + b
+#print = (sum) 
+
+# 리스트 관련 함수
+
+
+#st = [1,3,5,7,9]
+#st.remove(1) # 리스트 안에 있는 값 삭제
+
+#num = len(st)
+
+#print(st)
+
+#st = [1,2,3]
+#st.append(4) # 리스트 안의 마지막 요소에 값을 집어넣음
+#print(st)
+
+#st.extend([5,6]) # [1, 2, 3, 4,+[ 5, 6]
+#print(st)
+
+#st = [1,2,4]
+#st.insert(2,3) # 인덱스 값 2의 위치에 3을 저장
+
+#print(st)
+
+#st [1,9]
+#st = []
+#st.append(1)
+#st.append(2)
+
+#st.insert(0,1)
+#st.insert(1,2)
+
+#print(st)
+
+
+#st = [1,2,3,4,5]
+#print(st.pop(1)) #인덱스 값 0의 위치에 저장된 데이터 삭제
+
+#print(st)
+#st.remove(5)
+#print(st)
+
+#st. remove(3)
+#print(st)
+
+# 검색함수들
+#st = [1,2,3,1,2]
+#st.count(1)
+#st.index(2) # 처음 2 등장하는 index 위치
+
+#print (st[st.index(2)])
+
+#string = "YoonSungWoo"
+# oo의 개수는
+
+#print(string.count('oo'))
+
+#org = "kim"
+
+#lcp = org.lower() # 모든 문자를 소문자로
+#ucp = org.upper() # 모든 문자를 대문자로
+
+#print(lcp)
+#print(ucp)
+
+org = " MIDDLE "
+
+# 타 언어에서는 trim()
+#cp1 = org.strip() # 앞뒤 공백 제거
+#cp2 = org.lstrip() # 왼쪽 공백 제거
+#cp3 = org.rstrip() # 오른쪽 공백 제거
+
+#print(cp1)
+#print(cp2)
+#print(cp3)
+#print(org)
+
+#org = "heoo"
+#rps = org.replace("he","ho") # 문자열 교체 함수
+#print(rps)
+
+#rps = org.replace("heo","")
+#print(rps)
+
+#org="Heee"
+
+#rps = org.replace("e","o",1) # 첫번째 e만 바꾼다
+#print(rps)
+
+#rps = org.replace("e","o",2) # 두개를 o로 바꾼다
+#print(rps)
+
+
+#org = "ab_cd_ef"
+
+#ret = org.split('_') # 글자 자르는데 return 값이 list
+#print(ret)
+
+#for string in ret:
+#    print (string)
+
+#tel = "020-1234-5678"
+#org = "020_1234_5678"
+#ret = org.split('_')
+#print(ret)
+
+#for string in ret:
+#    print (string)
+
+# 탐색 관련
+
+#string = "What is important is that you should choose what is best for you"
+
+#string.find("is") #index 값이 리턴됨
+#string.rfind("is")#마지막 is가 있는 index값이 리턴됨
+#string.count("is")#개수
+
+#이스케이프(탈출)문자
+
+string = "escape ₩n characters"
+print(string)
+
+string = "escape ₩/tcharacters"
+print(string)
+
+#두가지 정리
+print("abcd",end="\n")
+print("abcd")
+
+# 두번째
+#string = "escape '안녕하세요' characters"
+#print(string)
+
+#string = "escape '안녕하세요' characters"
+#print(string)
+
+#함수가 아닌 del(삭제) 명령
+
+
+#st = [1,2,3,4,5]
+#del st[:]
+#del st[0]
+#print(st)
+
+#del st[1:3]
+#print(st)
+
+#del st
+#print(st)
+#num = 3
+#del num
+#print(num)
+
 
---
+++ practice/조건문 연습.py
@@ -0,0 +1,67 @@
+# bool형 
+#True
+#print(True)
+#print(type(True))
+
+#False
+#print(False)
+#print(type(False))
+
+
+#cpu에게 묻는 것 -> 답을 bool형으로 준다
+
+# 3 > 10 
+# 3 + 10
+
+###데이터 타입의 진정한 의미는
+###메모리에 값을 올릴 때도 언어 차원에서 제공하는 데이터 타입 중 한개로 올라가고 = 개발자가 하나를 정해서 데이터를 올
+###cpu가 답을 줄 때도 데이터 타입 중 하나를 잡아서 응답을 준다
+
+# if 문 연습 조건이 맞으면 실행
+
+#num = int(input("정수 입력"))
+
+#if num > 0 : # True False가 리턴되는 구문
+#    print("양수")
+
+#if num != 0 : # ==같으면 #!=다르면
+#    print("0이 아닙니다")
+
+
+# if else 구문은 반드시 한개는 실행됨
+#num = int(input("정수 입력"))
+
+#if num > 0 : 
+#    print("양수")
+#else:
+#    print("음수")
+
+
+
+#def main():
+#        pnum = input("스마트폰 번호 입력:")
+#        if pnum.isdigit() and pnum.startswith("010"):
+#            print("전화번호가 유효합니다.")
+#        else:
+#            print("전화번호가 유효하지 않습니다.")
+
+#main()
+
+#def main():
+#        s = "tomato spaghetti"
+#        if s. find("ghe") != -1:
+#            print("있습니다 .")
+#        else:
+#            print("없습니다.")
+
+#main()
+
+#def main():
+#        s = "tomato spaghetti"
+#        if "ghe" in s:
+#            print("있습니다 .")
+#        else:
+#            print("없습니다.")
+
+#main()
+
