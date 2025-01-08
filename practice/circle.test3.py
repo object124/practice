51ce482f943c751347ac30dddcc699dc6f1f93ea
object124 <zecc4504@gmail.com>
2025-01-08 15:47:55 +0900

    Add files via upload

---
+++ practice/circle.test3.py
@@ -0,0 +1,7 @@
+from circle import ci_circle as cc
+
+def ci_circle(r):
+    print("hello"+str(r))
+print(cc(4))
+
+ci_circle(4)
---
+++ practice/circle.test4.py
@@ -0,0 +1,6 @@
+import circle as cc
+
+r = float(input(" 반지름 입력 : "))
+ar = cc.ar_circle(r)
+print(" 원의 넓이 : ", ar)
+
